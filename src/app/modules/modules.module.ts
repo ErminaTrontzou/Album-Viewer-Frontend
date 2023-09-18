4919f8359577e7e0b3084d2c28f93d31d7ab9d8c
Ermina Trontzou <7erminatrontzou7@gmail.com>
2023-09-18 14:44:42 +0300

    Finished the design of the landing page

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -1,15 +1,18 @@
 import { CategoriesComponent } from './components/categories/categories.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AboutComponent } from './components/about/about.component';
 
 
 
 @NgModule({
   declarations: [
-    CategoriesComponent
+    CategoriesComponent,
+    AboutComponent
   ],
   exports: [
-    CategoriesComponent
+    CategoriesComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule
