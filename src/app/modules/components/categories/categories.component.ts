1f919efc15eea1a500d336992d9df793baf16007
Ermina Trontzou <7erminatrontzou7@gmail.com>
2023-10-25 19:21:16 +0300

    Implemented categories (genre) component

--- src/app/modules/components/categories/categories.component.ts
+++ src/app/modules/components/categories/categories.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { CategoriesService } from '../../services/categories-service';
 
+
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit{
-  categories: string[] = [];
+  categories: any[] = [
+    { name: 'Category 1', picture: 'path-to-image-1.jpg' },
+  { name: 'Category 2', picture: 'path-to-image-2.jpg' },
+  ];
   errorMessage: string = ''; 
   constructor(private categoriesService: CategoriesService) {}
 
@@ -18,11 +22,13 @@ export class CategoriesComponent implements OnInit{
   fetchCategories() {
     this.categoriesService.getCategories().subscribe(
       (data) => {
+        console.log('Fetched data:', data);
         this.categories = data;
       },
       (error) => {
-        this.errorMessage = 'Problem with obtaining categories from backend'; 
+        this.errorMessage = 'Problem with obtaining categories from the backend';
       }
     );
   }
+  
 }
